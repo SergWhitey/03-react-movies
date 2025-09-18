e6ec87bcbc1e8646ea99fb9e2d763a5506efcc60
SergWhitey <truesteell@gmail.com>
2025-09-19 00:27:44 +0300

    1

--- src/components/MovieModal/App/App.tsx
+++ src/components/MovieModal/App/App.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { toast } from 'react-hot-toast';
-import { fetchMovies } from './services/movieService';
-import { Movie } from './types/movie';
-import SearchBar from './components/SearchBar/SearchBar';
-import MovieGrid from './components/MovieGrid/MovieGrid';
-import Loader from './components/Loader/Loader';
-import ErrorMessage from './components/ErrorMessage/ErrorMessage';
-import MovieModal from './components/MovieModal/MovieModal';
+import { fetchMovies } from '../../../services/movieService';
+import { Movie } from '../../../types/movie';
+import SearchBar from '../../SearchBar/SearchBar';
+import MovieGrid from '../../MovieGrid/MovieGrid';
+import Loader from '../../Loader/Loader';
+import ErrorMessage from '../../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal';
 const token = import.meta.env.VITE_TMDB_TOKEN;
 import './index.css'; 
 
